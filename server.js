e533c34285575df574d9d1ce682394b3a2934dec
Jorge Rossi <jorgefzrossi@gmail.com>
2019-03-17 15:19:48 -0300

    Removing source files from root dir

--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app');
-
-const host = process.env.USE_DOCKER === 'true' ? process.env.HOST : '127.0.0.1';
-const port = process.env.USE_DOCKER === 'true' ? process.env.PORT : 3000;
-
-app.listen(port, host, () => {
-  console.log(`Server running at ${host}:${port}`);
-});
