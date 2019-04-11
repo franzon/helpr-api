b38779250304409891cc8a3df37c2b00196d7801
Jorge Rossi <jorgefzrossi@gmail.com>
2019-04-11 15:35:13 -0300

    Fixing heroku error

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 require('./database/database');
 const app = require('./app');
 
-const host = process.env.USE_DOCKER === 'true' ? process.env.HOST : '0.0.0.0';
-const port = process.env.USE_DOCKER === 'true' ? process.env.PORT : 3000;
+// const host = process.env.USE_DOCKER === 'true' ? process.env.HOST : '0.0.0.0';
+// const port = process.env.USE_DOCKER === 'true' ? process.env.PORT : 3000;
+
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || '0.0.0.0';
 
 app.listen(port, host, () => {
   // eslint-disable-next-line no-console
