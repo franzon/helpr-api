7068441cbe8cdf755e332f09a948e1abf0a89372
ogoes <otaviosilva820@gmail.com>
2019-05-03 22:52:24 -0300

    test login

---
+++ src/domains/authentication/login.js
@@ -0,0 +1,48 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const joi = require('joi');
+const models = require('../../database/models');
+const { validateRequest, regexes } = require('../../utils/validation');
+const keys = require('../../utils/keys.json');
+
+async function login(req, res) {
+  const schema = {
+    body: {
+      email: joi
+        .string()
+        .regex(regexes.email)
+        .required(),
+      password: joi
+        .string()
+        .regex(regexes.password)
+        .required(),
+    },
+  };
+
+  const error = validateRequest({ body: req.body }, schema);
+  if (error !== null) {
+    return res.status(400).json({ message: 'validation fail', data: null });
+  }
+
+  const { email, password } = req.body;
+
+  const user = await models.User.findOne({ email });
+  
+  if (user !== null) {
+    if (await bcrypt.compareSync(password, user.password)) {
+      const token = await jwt.sign(user.email, keys.jwt);
+      return res.status(200).json({
+        message: 'sucess',
+        data: {
+          name: user.name,
+          token,
+        },
+      });
+    }
+    return res.status(400).json({ message: 'invalid password', data: null });
+  }
+
+  return res.status(400).json({ message: 'user doesnt exist', data: null });
+}
+
+module.exports = { login };
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
 const emailController = require('../domains/authentication/email');
+const loginController = require('../domains/authentication/login');
 
 router.post('/email/sendConfirmationCode', emailController.sendConfirmationCode);
 router.post('/email/confirmEmail', emailController.confirmEmail);
+router.post('/login', loginController.login);
 
 module.exports = router;
