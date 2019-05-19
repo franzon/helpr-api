dabc472d82d16a84b6c8aceb21d988b0d7669f90
dennisurtubia <dennisurtubia@alunos.utfpr.edu.br>
2019-05-19 05:40:54 -0300

    Created categories and activities provider models and updated provider model

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -1,9 +1,13 @@
 const User = require('./user');
 const EmailConfirmation = require('./emailConfirmation');
 const Provider = require('./provider');
+const ActivitiesProvider = require('./activitiesProvider');
+const CategoriesProvider = require('./categoriesProvider');
 
 module.exports = {
   User,
   EmailConfirmation,
   Provider,
+  ActivitiesProvider,
+  CategoriesProvider,
 };
